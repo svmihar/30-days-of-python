eafe85de8c27a5a05e259753b545dbbd1b7fad06
svmihar <svmihar@gmail.com>
2018-11-29 17:37:55 +0700

    added whatsapp bot using selenium

---
+++ whatsapp-spam-bot/kill.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://web.whatsapp.com/')
+
+name = input('Enter the name of user or group : ')
+msg = input('Enter your message : ')
+count = int(input('Enter the count : '))
+
+input('Enter anything after scanning QR code')
+
+user = driver.find_element_by_xpath('//*[@id="pane-side"]/div/div/div/div[1]/div/div/div[2]/div[1]/div[1]/span'.format(name))
+user.click()
+
+msg_box = driver.find_element_by_class_name('_3F6QL')
+
+for i in range(count):
+    msg_box.send_keys(msg)
+    button = driver.find_element_by_class_name('_35EW6')
+    button.click()
