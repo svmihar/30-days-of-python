19b909c9ab96aa753e5850143afc2e6faab87390
svmihar <svmihar@gmail.com>
2019-02-09 17:50:53 +0700

    add pickle model

--- keras-deeplearning-sentdex/02-catsdogs.py
+++ keras-deeplearning-sentdex/02-catsdogs.py
@@ -1,13 +1,12 @@
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt 
-import os 
-import cv2
+import os, cv2, random
+from tqdm import tqdm 
 from keras.preprocessing.image import ImageDataGenerator, load_img
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-import random
 
 # CATEGORIES = ['Dog','Cat']
 # DATADIR='dataset/PetImages'
@@ -21,27 +20,64 @@ import random
 #     break 
 
 import os 
-print(os.listdir("dataset/"))
 CATEGORIES = ['Dog','Cat']
-DATADIR = "dataset/PetImages/"
+DATADIR = r"C:\\Users\\PEMODELAN-01\\Downloads\\tian\\30-days-of-python\\keras-deeplearning-sentdex\\dataset\\PetImages\\"
 
 FAST_RUN = False
 IMAGE_WIDTH = 128
 IMAGE_HEIGHT = 128
-IMAGE_SIZE = (IMAGE_WIDTH,IMAGE_HEIGHT)
+IMG_SIZE = (IMAGE_WIDTH,IMAGE_HEIGHT)
 IMAGE_CHANNELS = 3
+MODEL_NAME = 'dogs-cats-{}-{}.model'.format('LR','2conv')
 
-for category in CATEGORIES:
-    path = os.path.join(DATADIR,category)
-    for img in os.listdir(path):
-        img_array=cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
-        plt.imshow(img_array, cmap='gray')
-        plt.show()
-        break
-    break
-IMG_SIZE = 65
-new_array = cv2.resize(img_array,(IMG_SIZE,IMG_SIZE))
-plt.imshow(new_array, cmap='gray')
-plt.show()
+def label_img(img):
+    word_label = img.split('.')[-3]
+    if word_label == 'cat': return [1,0]
+    elif word_label == 'dog' : return [0,1]
 
-training_data= []
+training_data = []
+
+IMG_SIZE=70
+def create_traindata(): 
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+        for img in tqdm(os.listdir(path)):
+            try:
+                img_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                # new_array = cv2.resize(img_array, (IMAGE_SIZE, IMAGE_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e: 
+                pass 
+                print(e)
+create_traindata()
+
+print(len(training_data))
+
+random.shuffle(training_data)
+
+for sample in training_data:
+    print(sample[1])
+X = [] 
+y = []
+
+for features, label in training_data:
+    X.append(features)
+    print("features: ", features)
+    y.append(label)
+    print("labels: ",label)
+
+print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE,1))
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+import pickle 
+pickle_out = open('X.pickle','wb')
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open('y.pickle','wb')
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+
---
+++ keras-deeplearning-sentdex/03-convolution-NN.py
@@ -0,0 +1,30 @@
+# find edges, and lines, then pooling. 
+# squares, circles, small features. 
+import tensorflow as tf 
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+
+#don't have the model 
+X = pickle.load(open("X.pickle","rb")) 
+y = pickle.load(open("y.pickle","rb"))
+
+X = X/255
+model = Sequential()
+model.add(Conv2D(64,(3,3),input_shape = X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss="binary_crossentropy",optimizer='adam',metrics=['accuracy']) #change loss to categorical 
+model.fit(X,y,batch_size=32, validation_split=0.15, epochs=10) #calculate 32 batches at once, depends on the size of the data
+
