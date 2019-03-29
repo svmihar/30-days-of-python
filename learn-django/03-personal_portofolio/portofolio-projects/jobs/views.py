64d03a600e9a20e270f0c86e37c16b9b0a82eaaa
svmihar <svmihar@gmail.com>
2019-03-29 22:48:19 +0700

    WIP static files for dependencies of bootstrap

--- learn-django/03-personal_portofolio/portofolio-projects/jobs/views.py
+++ learn-django/03-personal_portofolio/portofolio-projects/jobs/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'jobs/ngentot.html') 
+
+def cantik(request): 
+    return render(request, 'template_/index.html')
--- learn-django/03-personal_portofolio/portofolio-projects/portofolio/settings.py
+++ learn-django/03-personal_portofolio/portofolio-projects/portofolio/settings.py
@@ -123,7 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static')
+
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
