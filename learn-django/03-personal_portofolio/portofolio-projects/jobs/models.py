e6d5534e6106b3ee8fcce011feb982c47d154721
svmihar <svmihar@gmail.com>
2019-03-30 07:59:05 +0700

    added jobs with bootstrap connected to database

--- learn-django/03-personal_portofolio/portofolio-projects/jobs/models.py
+++ learn-django/03-personal_portofolio/portofolio-projects/jobs/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200) # tiap database harus punya max length biar uniform tiap tablenya
+    #job_title = models.CharField(max_length=40)
--- learn-django/03-personal_portofolio/portofolio-projects/jobs/views.py
+++ learn-django/03-personal_portofolio/portofolio-projects/jobs/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 def home(request):
-    return render(request, 'jobs/ngentot.html') 
+    jobs = Job.objects
+    return render(request, 'jobs/ngentot.html', {'jobs': jobs}) 
 
 def cantik(request): 
     return render(request, 'template_/index.html')
