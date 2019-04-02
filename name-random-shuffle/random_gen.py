7eecda4195ab3a14e48d295451e5dd632be88cc0
svmihar <svmihar@gmail.com>
2019-04-02 11:48:26 +0700

    added 33 element distributed to 10 groups with random number for the task in dicitonary

--- name-random-shuffle/random_gen.py
+++ name-random-shuffle/random_gen.py
@@ -1,49 +1,50 @@
-from bs4 import BeautifulSoup
-
-# with open('this.html','rb') as f: 
-#     content = f.read()
-# soup = BeautifulSoup(content,'html.parser')
-# names = soup.find_all('td')
-# for name in names: 
-#     print(name.text)
-blm = '«BLM MENGISI KUESIONER»'
+from pprint import pprint
+import random
 
 with open('kumpulan.txt','rb') as f: 
     contents = f.readlines()
+
+blm = '«BLM MENGISI KUESIONER»'
 nama = []
 for content in contents:
     content = str(content,'utf-8')
-    # print(type(content))
-    # print(content)
     if 'MENGISI' in content: 
-        # print(content)
         nama.append(content[:-24].upper())
 
-print(nama)
-awal = 43
-akhir = 78
-nomor = []
-for i in range(34): 
-    awal+=1
-    nomor.append('4.'+str(awal))
+#remove karena gak pernah masuk
+nama.remove('MUHAMAD FARRAS HANINDITO RUKMANTORO')
 
-print('jumlah angka: ',len(nomor))
-print('jumlah nama:', len(nama))
-
-
-
-from random import shuffle,randint
-import pprint
-random_number = randint(1,100)
-for i in range(random_number):
-    shuffle(nama)
-    # shuffle(nomor)
-    dict={}
-    for angka, names in zip(nomor,nama):
-        dict[angka] = [names]
-
-pprint.pprint(dict)
-print('number of iteration: ',random_number)
+soal = [1,1,2,2,3,3,4,4,5,5]
+dict_soal = {
+    '1': 'TEORI PERSEDIAAN',
+    '2': 'TEORI ANTRIAN',
+    '3': 'TEORI PERMAINAN',
+    '4': 'GOAL PROGRAMMING',
+    '5': 'DYNAMIC PROGRAMMING'
+    }
+random.shuffle(nama)
+random.shuffle(soal)
 
 
+kelompok = []
+for i in range(0,len(nama),3):
+    kelompok.append(nama[i:i+3])
+anggota_sisa = kelompok[-1]
+print(f'tersisa: {anggota_sisa}')
+kelompok.remove([anggota for anggota in anggota_sisa])
 
+daleman = dict()
+fix = dict()
+for i in range(len(kelompok)): 
+    fix[i+1]= {
+        'anggota kelompok': kelompok[i],
+        'soal': dict_soal.get(str(soal[i])),
+        }
+
+for anggota in anggota_sisa: 
+    angka = random.randint(0,len(fix))
+    print(f'memasukkan {anggota} ke {angka}')
+    fix[angka]['anggota kelompok'].append(anggota)
+
+
+pprint(fix)
