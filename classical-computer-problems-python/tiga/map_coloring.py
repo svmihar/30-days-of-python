8d87f95edb71d4886128d0c0b555e92e5f786cd5
svmihar <svmihar@gmail.com>
2019-06-03 23:27:55 +0700

    pep8

--- classical-computer-problems-python/tiga/map_coloring.py
+++ classical-computer-problems-python/tiga/map_coloring.py
@@ -1,14 +1,15 @@
 from csp import MConstraint, CSP
-from typing import Dict, List, Optional 
+from typing import Dict, List, Optional
 
-class MapColoringConstraint(Constraint[str, str]): 
-    def __init__(self, place1: str, place2: str)-> None: 
+
+class MapColoringConstraint(Constraint[str, str]):
+    def __init__(self, place1: str, place2: str) -> None:
         super().__init__([place1, place2])
         self.place1: str = place1
         self.place2: str = place2
 
-    def satisfied(self, assignment: Dict[str, str]) -> bool: 
-        if self.place1 not in assignment or self.place2 not in assignment: 
+    def satisfied(self, assignment: Dict[str, str]) -> bool:
+        if self.place1 not in assignment or self.place2 not in assignment:
             return True
         return assignment[self.place1] != assignment[self.place2]
 
@@ -20,13 +21,18 @@ if __name__ == "__main__":
     for variable in variables:
         domains[variable] = ["red", "green", "blue"]
     csp: CSP[str, str] = CSP(variables, domains)
-    csp.add_constraint(MapColoringConstraint("Western Australia", "Northern Territory"))
-    csp.add_constraint(MapColoringConstraint("Western Australia", "South Australia"))
-    csp.add_constraint(MapColoringConstraint("South Australia", "Northern Territory"))
-    csp.add_constraint(MapColoringConstraint("Queensland", "Northern Territory"))
+    csp.add_constraint(MapColoringConstraint(
+        "Western Australia", "Northern Territory"))
+    csp.add_constraint(MapColoringConstraint(
+        "Western Australia", "South Australia"))
+    csp.add_constraint(MapColoringConstraint(
+        "South Australia", "Northern Territory"))
+    csp.add_constraint(MapColoringConstraint(
+        "Queensland", "Northern Territory"))
     csp.add_constraint(MapColoringConstraint("Queensland", "South Australia"))
     csp.add_constraint(MapColoringConstraint("Queensland", "New South Wales"))
-    csp.add_constraint(MapColoringConstraint("New South Wales", "South Australia"))
+    csp.add_constraint(MapColoringConstraint(
+        "New South Wales", "South Australia"))
     csp.add_constraint(MapColoringConstraint("Victoria", "South Australia"))
     csp.add_constraint(MapColoringConstraint("Victoria", "New South Wales"))
     csp.add_constraint(MapColoringConstraint("Victoria", "Tasmania"))
--- classical-computer-problems-python/tiga/queens.py
+++ classical-computer-problems-python/tiga/queens.py
@@ -1,33 +1,34 @@
-from csp import Constraint, CSP 
-from typing import Dict, List, Optional 
+from csp import Constraint, CSP
+from typing import Dict, List, Optional
 
-class QueensConstraint(Constraint[int, int]): 
-    def __init__(self, columns): 
+
+class QueensConstraint(Constraint[int, int]):
+    def __init__(self, columns):
         super().__init__(columns)
         self.columns = columns
 
-    def satisfied(self, assignment) -> bool: 
-        for q1c, q1r in assignment.items(): 
-            for q2c in range(q1c+1,  len(Self.columns)+1): 
-                if q2c in assignment: 
-                    q2r :int = assignment[q2c]
-                    if q1r == q2r: 
-                        return False 
-                    if abs(q1r - q2r) == abs(q1c - q2c): 
-                        return False 
+    def satisfied(self, assignment) -> bool:
+        for q1c, q1r in assignment.items():
+            for q2c in range(q1c+1,  len(Self.columns)+1):
+                if q2c in assignment:
+                    q2r: int = assignment[q2c]
+                    if q1r == q2r:
+                        return False
+                    if abs(q1r - q2r) == abs(q1c - q2c):
+                        return False
         return True
 
 
 if __name__ == "__main__":
-    columns = [x for x in range(1,9)]
+    columns = [x for x in range(1, 9)]
     rows = {}
-    for column in columns: 
-        rows[column] = [x for x in range(1,9)]
-    csp: CSP[int, int]=CSP(columns, rows)
+    for column in columns:
+        rows[column] = [x for x in range(1, 9)]
+    csp: CSP[int, int] = CSP(columns, rows)
 
     csp.add_constraint(QueensConstraint(columns))
     solution = csp.backtracking_search()
-    if solution is None: 
+    if solution is None:
         print('no solution')
-    else: 
+    else:
         print(solution)
