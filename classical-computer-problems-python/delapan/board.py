34f248fb9940733ce054ef42c43778924fcc0962
svmihar <svmihar@gmail.com>
2019-06-28 21:00:50 +0700

    chapter 8 framework, WIP tictactoe class

---
+++ classical-computer-problems-python/delapan/board.py
@@ -0,0 +1,37 @@
+from  __future__ import annotations
+from typing import NewType. List
+from abc import ABC, abstractmethod
+
+Move = NewType('Move', int)
+
+class Piece: 
+    @property
+    def opposite(self)-> Piece: 
+        raise NotImplementedError('should be implemented by subclass! ')
+
+class Board: 
+    @property
+    @abstractmethod
+    def turn(self) -> Piece: 
+        ...
+
+    @property
+    @abstractmethod
+    def legal_moves(self) -> List[Move]: 
+        ...
+
+    @property
+    @abstractmethod
+    def is_win(self) -> bool: 
+        ...
+
+    @property
+    def is_draw(self) -> bool: 
+        return (not self.is_win) and(len(self.legal_moves) == 0)
+    
+    @abstractmethod
+    def evaluate(self, player: Piece) -> float: 
+        ...
+
+
+
---
+++ classical-computer-problems-python/delapan/tictactoe.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import List
+from enum import Enum 
+from board import Piece, Board, Move
+
+
+class TTTPiece(Piece, Enum): 
+    X = 'X'
+    O = 'O'
+    E = " " # empty state
+
+    @property
+    def opposite(self) -> TTTPiece :
+        if self == TTTPiece.X: 
+            return TTTPiece.O
+        elif self == TTTPiece.O: 
+            return TTTPiece.X
+        else: 
+            return TTTPiece.E
+    
+def __str__(self) -> str: 
+    return self.value
+    
