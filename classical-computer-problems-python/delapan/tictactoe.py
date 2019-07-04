9265b087487fd33b52948f0ae004d0fe13611d9f
svmihar <svmihar@gmail.com>
2019-07-04 15:23:15 +0700

    added catatan, dan finished tictactoe framework

--- classical-computer-problems-python/delapan/tictactoe.py
+++ classical-computer-problems-python/delapan/tictactoe.py
@@ -32,15 +32,28 @@ class TTTBoard(Board):
     def turn(self) -> Piece:
         # ada 2 turn, dan _turn biar tiap board tau siapa giliran siapa. which membingungkan. w
         return self._turn
-    
 
-    def move(self, location: Move) -> Board: 
-        # check all possible move without "switch turns", jadi state dimana dia di turn itu aja. 
+    def move(self, location: Move) -> Board:
+        # check all possible move without "switch turns", jadi state dimana dia di turn itu aja.
         temp_position: List[TTTPiece] = self.position.copy()
         temp_position[location] = self._turn
         return TTTBoard(temp_position, self._turn.opposite)
-    
+
     @property
-    def legal_moves(self) -> bool: 
-        # 3 row, 3 column and 2 diagonal check. 
-        return self.position[0] == self.position[1] and self.position[0] == self.position[2] and self.position[0] != TTTPiece.E or self.position[3] == self.position[4] and self.position[3] == self.position[5] and self.position[3] !=TTTPiece.E or self.position[6]  == self.position[7] and self.position[6] == self.position[8] and self.position[6] != TTTPiece.E or self.position[1] == self.position[4] and self.position[1] == self.position[7] and self.position[1] != TTTPiece.E or self.position[2] == self.position[5] and self.position[2] ==self.position[8] and self.position[2] != TTTPiece.E or self.position[0] == self.position[4] and self.position[0] == self.position[8] and self.position[0] != TTTPiece.E or self.position[2] == self.position[4] and self.position[2] == self.position[6] and self.position[2] != TTTPiece.E
+    def legal_moves(self) -> bool:
+        # initinya mencari kalo kolom, baris atau diagonal nya tidak kosong dan mempunyai isi yang sama ya berarti gamenya menang.
+        # gamenya drawn kalo tidak menang dan tidak ada lagi legal moves nya. dan sudah di cover di class boardnya.
+        # 3 row, 3 column and 2 diagonal check.
+        return self.position[0] == self.position[1] and self.position[0] == self.position[2] and self.position[0] != TTTPiece.E or self.position[3] == self.position[4] and self.position[3] == self.position[5] and self.position[3] != TTTPiece.E or self.position[6] == self.position[7] and self.position[6] == self.position[8] and self.position[6] != TTTPiece.E or self.position[1] == self.position[4] and self.position[1] == self.position[7] and self.position[1] != TTTPiece.E or self.position[2] == self.position[5] and self.position[2] == self.position[8] and self.position[2] != TTTPiece.E or self.position[0] == self.position[4] and self.position[0] == self.position[8] and self.position[0] != TTTPiece.E or self.position[2] == self.position[4] and self.position[2] == self.position[6] and self.position[2] != TTTPiece.E
+
+    def evaluate(self, player: Piece) -> float:
+        if self.is_win and self.turn == player:
+            return -1
+        elif self.is_win and self.turn != player:
+            return 1
+        else:
+            return 0
+    def __repr__(self) -> str: 
+        return f"""{self.position[0]} | {self.position[1]} | {self.position[2]}
+        {self.position[3]} | {self.position[4]} | {self.position[5]} 
+        {self.position[6]} | {self.position[7]} | {self.position[8]}"""
