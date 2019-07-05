b4a6809925445dd923a47a8e62e1b75698e56ace
svmihar <svmihar@gmail.com>
2019-07-05 21:00:48 +0700

    finished exercies

--- list-comprehension/exercises/generators.py
+++ list-comprehension/exercises/generators.py
@@ -1,12 +1,20 @@
+# %load exercises/generators.py
 """Generator Exercises."""
+import string
 
-
-def is_prime():
+def is_prime(number):
     """Return True if candidate number is prime."""
-
+#     for n in range(2, number): 
+#         if number % n ==0: 
+#             return False 
+#         return True
+    return all(
+        number % n != 0 
+        for n in range(2,number)
+              )
 
 def all_together():
-    """String together all items from the given iterables."""
+    """String together all items from the given iterables."""  
 
 
 def interleave():
@@ -25,5 +33,15 @@ def first_prime_over():
     """Return the first prime number over a given number."""
 
 
-def is_anagram():
+def is_anagram(word1,word2):
     """Return True if the given words are anagrams."""
+    return sorted([
+        x 
+        for x in word2.lower() 
+        if x in string.ascii_lowercase
+    ])==sorted([
+        x 
+            for x in word1.lower() 
+            if x in string.ascii_lowercase
+    ])
+    
