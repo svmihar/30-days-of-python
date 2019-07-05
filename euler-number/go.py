fd4c685ab73e0116a0e4933d0416a9502803a616
svmihar <svmihar@gmail.com>
2019-07-05 21:40:17 +0700

    update to use generator

--- euler-number/go.py
+++ euler-number/go.py
@@ -1,12 +1,13 @@
 # import os 
+from itertools import count
 
-e = 0
-n=1
-while True: 
-    n+=1
-    e = (1+1/n)**n
-    if n>1120283900:
-        print(e)
-        print(f'iteration: {n} \nwith number {e}')
-        break
+# e = 0
+# n=1
+# while True: 
+#     n+=1
+#     e = (1+1/n)**n
     # os.system('clear')
+c = count()
+next(c)
+for n in c: 
+    print((1+1/n)**n)
